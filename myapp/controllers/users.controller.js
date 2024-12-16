bcfa4a18c672642ab44ef02fecd5e546a95791dd
Nylun <kawzex@outlook.fr>
2024-12-16 21:20:13 +0100

    [+]Adding CRUD for user

--- myapp/controllers/users.controller.js
+++ myapp/controllers/users.controller.js
@@ -29,9 +29,64 @@ const createUser = async (req, res) => {
       }
 }
 
-// Lecture des données client
+// Lecture des données clients (général)
 
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+// Lecture des données clients (par id)
+
+const getUsers = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id)
+        res.status(200).json(user)
+      } catch (error) {
+        res.status(500).json({message: error.message});
+      }
+};
+
+// Mise à jour des données clients
+
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndUpdate(id, req.body);
+        if (!user) {
+          return res.status(404).json({message: "utilisateur non trouvé"});
+        }
+        const updatedUser = await User.findById(id);
+        res.status(200).json(updateUser)
+      } catch (error) {
+        res.status(500).json({message: error.message});
+      }
+}
+
+//Suppression d'un user
+
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndDelete(id);
+        if (!user) {
+          return res.status(404).json({message: "Utilisateur introuvable"})
+        }
+        res.status(200).json({message: "utilisateur supprimé"})
+      } catch (error) {
+        res.status(500).json({message: error.message});
+      }
+}
 
 module.exports = {
-    createUser
+    createUser,
+    getAllUsers,
+    getUsers,
+    updateUser,
+    deleteUser
 };
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,8 +2,18 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-const {createUser} = require('../controllers/users.controller') 
+const {createUser, getAllUsers, getUsers, updateUser, deleteUser} = require('../controllers/users.controller') 
 
+
+//Création d'un user
 router.post('/add', createUser);
+//Récupérartion de tous les users
+router.get('/', getAllUsers);
+//Récupération des données d'un utilisateur
+router.get('/:id', getUsers)
+//Mise à jour des données d'un utilisateur
+router.put('/:id', updateUser)
+//Suppression d'un User
+router.delete('/:id', deleteUser)
 
 module.exports = router;
