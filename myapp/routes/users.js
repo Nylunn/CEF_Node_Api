960541318bd9f8ab8280c64d9b85ebe1431e7d1a
Nylun <kawzex@outlook.fr>
2025-01-21 19:26:13 +0100

    [/]Trying to fix tests...

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,23 +1,28 @@
-// Dans users.js ou routes/users.js
-const express = require('express');
-const User = require('../models/user');
+const express = require("express");
+const User = require("../models/user");
 const router = express.Router();
-const {createUser, getAllUsers, getUsers, updateUser, deleteUser, authenticate} = require('../controllers/users.controller') 
-const private = require('../middlewares/private')
-
+const {
+  createUser,
+  getAllUsers,
+  getUsers,
+  updateUser,
+  deleteUser,
+  authenticate,
+} = require("../controllers/users.controller");
+const private = require("../middlewares/private");
 
 //Création d'un user
-router.post('/add',  createUser);
+router.post("/add", createUser);
 //Récupérartion de tous les users
-router.get('/', private.checkJWT, getAllUsers);
+router.get("/", private.checkJWT, getAllUsers);
 //Récupération des données d'un utilisateur
-router.get('/:id', private.checkJWT, getUsers)
+router.get("/:id", private.checkJWT, getUsers);
 //Mise à jour des données d'un utilisateur
-router.put('/:id', private.checkJWT, updateUser)
+router.put("/:id", private.checkJWT, updateUser);
 //Suppression d'un User
-router.delete('/:id', private.checkJWT, deleteUser)
+router.delete("/:id", private.checkJWT, deleteUser);
 
 //Connexion à un compte
-router.post('/authenticate',  authenticate)
+router.post("/authenticate", authenticate);
 
 module.exports = router;
--- myapp/test/register_test.js
+++ myapp/test/register_test.js
@@ -1,25 +1,25 @@
 import * as chai from "chai";
 import chaiHttp from "chai-http";
-import { expect } from "chai";
-import app from "../express_app/app.js";
 
 chai.use(chaiHttp);
+import { expect } from "chai";
+import server from "../express_app/app.js";
 
-describe("User Registration", () => {
-  it("should register a new user successfully", async () => {
-    const user = {
-      name: "John Doe",
-      email: "johndoe@example.com",
-      password: "securePassword123",
-    };
-
-    const res = await chai.request(app).post("/users/add").send(user);
-
-    expect(res).to.have.status(201);
-    expect(res.body).to.be.an("object");
-    expect(res.body).to.have.property(
-      "message",
-      "User registered successfully"
-    );
+describe("POST /users/add", () => {
+  it("should create a new user", async (done) => {
+    this.timeout(10000);
+    await request("https://localhost:3000/")
+      .post("/users/add")
+      .send({
+        username: "john",
+        password: "johndoe",
+        email: "johndoe@gmail.com",
+      })
+      .end((err, res) => {
+        if (err) return done(err), expect(res).to.have.status(201);
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.equal("john");
+        done();
+      });
   });
 });
